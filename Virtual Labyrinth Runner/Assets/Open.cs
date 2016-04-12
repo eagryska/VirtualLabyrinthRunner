e778e620f8e79178f077f692666a1c4fcfc00688
scarlettuce <jyang75@illinois.edu>
2016-04-11 23:37:06 -0500

    gear room

---
+++ Virtual Labyrinth Runner/Assets/Open.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Open : MonoBehaviour {
+    public int counter;
+	// Use this for initialization
+	void Start () {
+        counter = 0;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (counter > 0)
+        {
+            return;
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("OPEN");
+            GetComponent<Animation>().Play("box_open");
+            counter += 1;
+        }
+
+    }
+}
